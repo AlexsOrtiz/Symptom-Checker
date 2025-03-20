0313b1f7c22547e7f99cdca3c873385e89c47ed5
Alexsandra Ortiz <107365664+AlexsOrtiz@users.noreply.github.com>
2025-03-20 10:36:04 -0500

    app.py

--- app.py
+++ app.py
@@ -105,4 +105,6 @@ def search():
     return jsonify(data_dict["symptoms"])
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
+
